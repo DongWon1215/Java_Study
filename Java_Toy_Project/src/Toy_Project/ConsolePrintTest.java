43a4bbbb716eccf1f49ecb21f0b92639c886ede0
동원 <53255275+DongWon1215@users.noreply.github.com>
2022-09-13 17:57:27 +0900

    homework init

---
+++ Java_Toy_Project/src/Toy_Project/ConsolePrintTest.java
@@ -0,0 +1,14 @@
+package Toy_Project;
+
+public class ConsolePrintTest 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+		System.out.println("안녕하세요 \t 감사합니다 \t 도발 많이받으세요 \n 좋은 카드가 너무 많아요");
+		System.out.println(" \'강조\' \"강한 강조\"");
+	}
+
+}
