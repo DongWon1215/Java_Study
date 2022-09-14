1765e13f905d0c6ad0e42a17f528da51b62220f8
dwkang <ejddjsl123@gmail.com>
2022-09-14 17:41:30 +0900

    self study

---
+++ Exam/Chapter03/src/Atrerisk.java
@@ -0,0 +1,21 @@
+
+public class Atrerisk {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			for(int i = 0; i < 5; i++)
+			{
+				for(int j = 5; j > 0; j--)
+				{
+					System.out.print(" ");
+				}
+				
+				System.out.print("*");
+				System.out.println();
+			}
+			/*
+			 * int i = 0; if( i !=-1) System.out.println("이건 되는구나");
+			 */
+	}
+
+}
--- Exam/Chapter03/src/Operator.java
+++ Exam/Chapter03/src/Operator.java
@@ -6,22 +6,15 @@ public class Operator
 	{
 		// TODO Auto-generated method stub
 		
-		//for(int i = 0; i < 10;i++)
-		//{
-		//	System.out.println(i);
-		//}
-		//int num = 0;
-		recursion(10);
+		recursion(0);
 	}
 	static void recursion(int i)
 	{
-		if(i == 0)
-			return;
-		//int m_iNum = i;
 		System.out.println("이건 재귀함수야");
 		System.out.println(i+"번째 반복되고 있지");
-		//m_iNum++;
-		recursion(i - 1);
+		if(i > 10)
+			return;
+		recursion(i + 1);
 		
 	}
 }
