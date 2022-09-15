948a4da1846e8cbbeb2299be0822217ca43a9d8c
dwkang <ejddjsl123@gmail.com>
2022-09-15 15:14:26 +0900

    Update Calculator.java

--- Exam/Calculator/src/Calculator.java
+++ Exam/Calculator/src/Calculator.java
@@ -1,10 +1,12 @@
 
 	import java.util.Scanner;
 	
-public class Calculator {
+public class Calculator 
+{
 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 				
