bfe497662044b10818693d7cf51cffff47bfbf86
dwkang <ejddjsl123@gmail.com>
2022-09-16 13:00:44 +0900

    Update Atrerisk.java

--- Exam/Chapter03/src/Atrerisk.java
+++ Exam/Chapter03/src/Atrerisk.java
@@ -27,11 +27,30 @@ public class Atrerisk {
 			System.out.println();
 			for(int i = 0; i < 5; i++)
 			{
-				for(int j = 0; j < i + 1; j++)
+				for(int k = 0;k < 4 - i; k++)
 				{
 					System.out.print(" ");
 				}
-				System.out.print("*");
+				for(int j = 0; j < i + 1; j++)
+				{
+					System.out.print("*");
+				}
+				//System.out.print("*");
+				
+				System.out.println();
+			}
+			System.out.println();
+			for(int i = 0; i < 5; i++)
+			{
+				for(int k = 0;k < i; k++)
+				{
+					System.out.print(" ");
+				}
+				for(int j = 5; j > i ; j--)
+				{
+					System.out.print("*");
+				}
+				//System.out.print("*");
 				
 				System.out.println();
 			}
