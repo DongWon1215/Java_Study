571a271a7131e7d56b1c3913e29563b3eb1e8efd
dwkang <ejddjsl123@gmail.com>
2022-09-19 15:32:52 +0900

    optimization

--- Exam/Chapter05/src/MemberInstance.java
+++ Exam/Chapter05/src/MemberInstance.java
@@ -1,14 +1,6 @@
 
 public class MemberInstance 
 {
-	public static void main(String[] args) 
-	{
-		
-	}
-}
-
-class Member
-{
 	private String strName;
 	private String strPhoneNumber;
 	private String strMajor;
@@ -17,7 +9,17 @@ class Member
 	private String strBirthDay;
 	private int iGrade;
 	
-	Member(String name, String phonenum, String major, String email, String address, String birthday, int grade)
+	MemberInstance(String name, String phonenum, String major, String email, int grade) 
+	{
+		strName = name;
+		strPhoneNumber = phonenum;
+		strMajor = major;
+		strEmail = email;
+		iGrade = grade;
+	}
+
+
+	MemberInstance(String name, String phonenum, String major, String email, String address, String birthday, int grade) 
 	{
 		strName = name;
 		strPhoneNumber = phonenum;
@@ -28,14 +30,26 @@ class Member
 		iGrade = grade;
 	}
 	
-	void Introduce()
+	void Introduce() 
 	{
-		System.out.println("제 이름은 " + strName + " 입니다");
-		System.out.println("제 전화번호는 " + strPhoneNumber + " 입니다");
-		System.out.println("제 전공은 " + strMajor + " 입니다");
-		System.out.println("제 학년은 " + iGrade + " 입니다");
-		System.out.println("제 이메일 주소는 " + strEmail + " 입니다");
-		System.out.println("제 집 주소는 " + strAddress + " 입니다");
-		System.out.println("제 생일은 " + strBirthDay + " 입니다");
+		if (strBirthDay != null && strAddress != null) 
+		{
+			System.out.println("제 이름은 " + strName + " 입니다");
+			System.out.println("제 전화번호는 " + strPhoneNumber + " 입니다");
+			System.out.println("제 전공은 " + strMajor + " 입니다");
+			System.out.println("제 학년은 " + iGrade + " 입니다");
+			System.out.println("제 이메일 주소는 " + strEmail + " 입니다");
+			System.out.println("제 집 주소는 " + strAddress + " 입니다");
+			System.out.println("제 생일은 " + strBirthDay + " 입니다");
+		}
+		
+		else
+		{
+			System.out.println("제 이름은 " + strName + " 입니다");
+			System.out.println("제 전화번호는 " + strPhoneNumber + " 입니다");
+			System.out.println("제 전공은 " + strMajor + " 입니다");
+			System.out.println("제 학년은 " + iGrade + " 입니다");
+			System.out.println("제 이메일 주소는 " + strEmail + " 입니다");
+		}
 	}
 }
---
+++ Exam/Chapter05/src/MemberInstanceMain.java
@@ -0,0 +1,22 @@
+
+public class MemberInstanceMain 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		MemberInstance mi = new MemberInstance("pykl.", ";ldgvs", "34trsdv", "pojkh", "vbmg", "qergh", 5);
+		MemberInstance mi2 = new MemberInstance("asd", "cxsd", "vftb", "trhgf", 3);
+		
+		mi.Introduce();
+		System.out.println();
+		mi2.Introduce();
+		
+		mi2 = mi;
+		
+		System.out.println(mi.toString());
+		System.out.println(mi2.toString());
+		System.out.println(mi.equals(mi2));
+	}
+
+}
