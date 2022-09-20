45d9fd625a5a9d91c306b3949e85b8ea96323ccb
dwkang <ejddjsl123@gmail.com>
2022-09-20 09:46:35 +0900

    folder relocation

---
+++ MiniProject/src/UserInfo.java
@@ -0,0 +1,65 @@
+
+public class UserInfo 
+{
+	private String name;
+	private String phoneNumber;
+	private String eMail;
+	private String address;
+	private String birthDay;
+	private String group;
+	
+	//===================Setter==============
+	void setName(String name)
+	{
+		this.name = name;
+	}
+	void setPhoneNumber(String phoneNumber)
+	{
+		this.phoneNumber = phoneNumber;
+	}
+	void setEMail(String eMail)
+	{
+		this.eMail = eMail;
+	}
+	void setAddress(String address)
+	{
+		this.address = address;
+	}
+	void setBirth(String birthDay)
+	{
+		this.birthDay = birthDay;
+	}
+	
+	void setGroup(String group)
+	{
+		this.group = group;
+	}
+	
+	//==================getter===============
+	
+	String getName()
+	{
+		return this.name;
+	}
+	String getPhoneNumber()
+	{
+		return this.phoneNumber;
+	}
+	String getEMail()
+	{
+		return this.eMail;
+	}
+	String getAddress()
+	{
+		return this.address;
+	}
+	String getBirth()
+	{
+		return this.birthDay;
+	}
+	
+	String getGroup()
+	{
+		return this.group;
+	}
+}
