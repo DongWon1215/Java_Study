a4cd273fa4db14ae3ea2a6b7d900145bf88eb0f0
dwkang <ejddjsl123@gmail.com>
2022-09-22 10:13:08 +0900

    Update SmartPhone.java

--- MiniProject/src/Ver02/SmartPhone.java
+++ MiniProject/src/Ver02/SmartPhone.java
@@ -8,7 +8,7 @@ class SmartPhone {
 	private Scanner sc = new Scanner(System.in);				// 확인 여부 확인용 스캐너
 	private Contact user[] = new Contact[MAX_PERSON_NUM];
 
-	private boolean isInNumRange(int index) 
+	private boolean isInNumRange(int index) 					// 저장된 데이터 갯수보다 더 높은 숫자를 입력했는지 확인
 	{
 		if(index < personCount)
 			return true;
@@ -16,7 +16,7 @@ class SmartPhone {
 		return false;
 	}
 	
-	private boolean isThisPerson(int index) 						//선택한 사람이 맞는지 
+	private boolean isThisPerson(int index) 					// 선택한 사람이 맞는지 확인
 	{
 		user[index].ShowInfo();
 		System.out.println("선택한 사람의 정보가 맞습니까?");
@@ -37,7 +37,7 @@ class SmartPhone {
 			return addressBook;
 	}
 	
-	public boolean searchPerson(String name)					// 입력한 이름과 동일한 이름을 가진 객체가 있는지 확인하는 함수
+	public boolean searchPerson(String name)					// 입력한 이름과 동일한 이름을 가진 객체가 있는지 확인
 	{
 		if(personCount == 0)
 			System.out.println("검색 할 데이터가 없습니다");
@@ -57,7 +57,7 @@ class SmartPhone {
 		return false;
 	}
 	
-	public boolean searchPeople()
+	public boolean searchPeople()								// 저장된 모든 객체 출력
 	{
 		for(int i = 0; i < personCount; i++)
 			{
@@ -71,7 +71,7 @@ class SmartPhone {
 		return false;
 	}
 	
-	public boolean addPerson(Contact ct)
+	public boolean addPerson(Contact ct)						// 객체를 배열에 추가
 	{
 		if(personCount >= MAX_PERSON_NUM)
 			System.out.println("최대 인원 수에 도달하였습니다");
@@ -85,7 +85,7 @@ class SmartPhone {
 		return false;
 	}
 	
-	public boolean deletePerson(int index)
+	public boolean deletePerson(int index)						// 배열에 저장된 객체 검색 후 삭제
 	{
 		if(personCount == 0)
 			System.out.println("지울 수 있는 데이터가 없습니다");
@@ -107,7 +107,7 @@ class SmartPhone {
 		return false;
 	}
 	
-	public boolean editPerson(int index, String name, String phoneNumber, String eMail, String address, String birth, String group)
+	public boolean editPerson(int index, String name, String phoneNumber, String eMail, String address, String birth, String group)		//배열에 저장된 객체의 정보를 수정
 	{
 
 		if(personCount == 0)
@@ -128,10 +128,5 @@ class SmartPhone {
 			}
 		}
 		return false;
-	}
-	
-	public int getAddressRealLength()
-	{
-		return personCount;
 	}
 }
