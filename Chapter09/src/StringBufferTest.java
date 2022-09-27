aef9079fc42c73f8cd07d07f91fa634ced3042a0
dwkang <ejddjsl123@gmail.com>
2022-09-27 17:59:16 +0900

    string class study

---
+++ Chapter09/src/StringBufferTest.java
@@ -0,0 +1,22 @@
+
+public class StringBufferTest 
+{
+	public static void main(String[] args) 
+	{
+		//StringBuffer, StringBuilder
+		//Buffer : 복수의 객체가 같은 버퍼를 사용(동기화 시켜야함 -> 한개의 객체가 버퍼를 점유하고 있으면 다른 객체들은 대기), 멀티스레드를 사용할 경우 버퍼를 사용하는것이 유리
+		//builder : buffer와 비슷하지만 단일 스레드에 최적화 되어있음
+		//문자열의 길이와 내용을 변경하는데 용이
+		
+		StringBuffer sb = new StringBuffer("");
+		
+		StringBuilder sbd = new StringBuilder("");
+		
+		sb.append("");
+		sb.delete(0,0);
+		sb.insert(0, "");
+		
+		
+		//동기화 하고자 한다면 synchronized 키워드를 사용
+	}
+}
--- Chapter09/src/StringTest.java
+++ Chapter09/src/StringTest.java
@@ -15,5 +15,25 @@ public class StringTest
 		String str5 = "" + 5;
 		System.out.println(str4);
 		System.out.println(str5);
+		
+		System.out.println(str1.concat());
+		
+		System.out.println(str1.startsWith("d"));
+		
+		System.out.println(str1.endsWith(".txt"));
+
+		System.out.println(str1.equals(str2));
+		
+		System.out.println(str1.contains("as"));
+		
+		System.out.println(str1.indexOf("i"));
+		System.out.println(str1.lastIndexOf("n"));
+		
+		System.out.println("".isEmpty());
+		System.out.println(" ".isEmpty());
+		System.out.println(" ".trim().isEmpty());
+		
+		System.out.println(str1.replace("g","g_false"));
+		
 	}
 }
