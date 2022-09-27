e3b356fbe656060cb7e4ac85dc54c46d174a7cfd
dwkang <ejddjsl123@gmail.com>
2022-09-27 12:04:23 +0900

    exam solve

--- Chapter08/src/InnerClassMain.java
+++ Chapter08/src/InnerClassMain.java
@@ -5,6 +5,7 @@ public class InnerClassMain {
 		// TODO Auto-generated method stub
 		OuterClass outer = new OuterClass("knull");
 		
+		//郴何 努贰胶 按眉 积己 规过
 		OuterClass.InnerClass inner = outer.new InnerClass();
 		
 	}
---
+++ Chapter08/src/exam/AbstractCalculator.java
@@ -0,0 +1,6 @@
+package exam;
+
+public abstract class AbstractCalculator implements Calculator
+{
+	private double PI = 3.14159265;
+}
