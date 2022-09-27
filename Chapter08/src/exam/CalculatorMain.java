803c4c91b484f0d75053309f25c77c89c90b4381
dwkang <ejddjsl123@gmail.com>
2022-09-27 16:14:28 +0900

    update chapter09

--- Chapter08/src/exam/CalculatorMain.java
+++ Chapter08/src/exam/CalculatorMain.java
@@ -1,5 +1,37 @@
 package exam;
 
 public class CalculatorMain {
-
+	
+	public static void main(String[] args) {
+		
+	CalculatorImplement calImp = new CalculatorImplement();
+	Calculator cal = new Calculator() {
+		
+		@Override
+		public long substract(long n1, long n2) {
+			// TODO Auto-generated method stub
+			return n1 + n2;
+		}
+		
+		@Override
+		public long multiply(long n1, long n2) {
+			// TODO Auto-generated method stub
+			return n1 - n2;
+		}
+		
+		@Override
+		public double divide(double n1, double n2) {
+			// TODO Auto-generated method stub
+			return n1 * n2;
+		}
+		
+		@Override
+		public long add(long n1, long n2) {
+			// TODO Auto-generated method stub
+			return n1 / n2;
+		}
+	};
+	
+	}
+	
 }
---
+++ Chapter09/src/Material.java
@@ -0,0 +1,23 @@
+
+public class Material extends Object{
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "null";
+	}
+	
+	public static void main(String[] args) {
+		
+		Material mt = new Material();
+		
+		System.out.println(mt.hashCode());
+	}
+	
+}
