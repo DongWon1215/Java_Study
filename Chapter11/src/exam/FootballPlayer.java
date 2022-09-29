49be41827facb5491b93db964b88508915d1afeb
dwkang <ejddjsl123@gmail.com>
2022-09-29 17:45:56 +0900

    WIP

--- Chapter11/src/exam/FootballPlayer.java
+++ Chapter11/src/exam/FootballPlayer.java
@@ -15,6 +15,51 @@ public class FootballPlayer
 		this.name = name;
 		this.team =team;
 	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "이름 : " + name + " 나이 : " + age +" 팀명 : " + team + " 등번호 : " + number;
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return ;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		
+		if(obj == null || !(obj instanceof FootballPlayer))
+			return false;
+		
+		FootballPlayer temp = (FootballPlayer)obj;
+		
+		if(this.number != temp.getNumber() ||
+				this.age != temp.getAge() ||
+				!this.name.equals(temp.getName()))
+			return false;
+		
+		return true;
+	}
 	
 	
 }
--- Chapter11/src/exam/FootballPlayerMain.java
+++ Chapter11/src/exam/FootballPlayerMain.java
@@ -1,7 +1,12 @@
 package exam;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
 
 
 public class FootballPlayerMain {
@@ -9,8 +14,36 @@ public class FootballPlayerMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		List<FootballPlayer> illustratedbook = new List<FootballPlayer>();
+		List<FootballPlayer> illustratedbook = new LinkedList<FootballPlayer>();
+		illustratedbook.add(new FootballPlayer(1,2,"aaa","bbb"));
+		illustratedbook.add(new FootballPlayer(2,1,"bbb","ccc"));
+		illustratedbook.add(new FootballPlayer(3,0,"ccc","ddd"));
+		for(FootballPlayer player : illustratedbook)
+			player.toString();
 		
+		
+		System.out.println();
+		
+		Set<FootballPlayer> playerSet = new HashSet<FootballPlayer>() ;
+		
+		
+		TreeSet<FootballPlayer> playerTreeSet = new TreeSet<FootballPlayer>();
+		playerTreeSet.add(new FootballPlayer(1,2,"aaa","bbb"));
+		playerTreeSet.add(new FootballPlayer(2,3,"aaa","bbb"));
+		playerTreeSet.add(new FootballPlayer(3,4,"ccc","ccc"));
+		playerTreeSet.add(new FootballPlayer(4,5,"ddd","ddd"));
+		playerTreeSet.add(new FootballPlayer(5,6,"eee","eee"));
+		playerTreeSet.add(new FootballPlayer(6,7,"fff","fff"));
+		
+		
+		
+		HashMap<Integer, FootballPlayer> playerMap = new HashMap<Integer, FootballPlayer>();
+		FootballPlayer a = new FootballPlayer(1,1,"aaa","aaa");
+		playerMap.put(a.getNumber(), a);
+		FootballPlayer b = new FootballPlayer(2,2,"bbb","bbb");
+		playerMap.put(b.getNumber(), b);
+		FootballPlayer c = new FootballPlayer(1,1,"aaa","aaa");
+		playerMap.put(c.getNumber(), c);
 	}
 
 }
